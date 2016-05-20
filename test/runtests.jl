ed25d8a7b1154187c5eda2e839a311758a2567dc
erantreister <erantreister@gmail.com>
2016-05-19 22:14:36 -0700

    trying to make travis run

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,3 @@ include("HelmholtzForwardTest.jl");
 include("ShiftedLaplacianTest.jl");
 
 
-#- julia -e 'Pkg.clone("https://github.com/JuliaInv/EikonalInv.jl","EikonalInv"); Pkg.build("EikonalInv");'
